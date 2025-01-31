0898366ac71224dcfb41b6a37dfa09a9524fe98a
Eden Awoke Amzaye <135725390+edenawoke9@users.noreply.github.com>
2025-01-31 23:07:35 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.10%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isSameAfterReversals(self, num):
+       self.num=num
+    
+       if num==0:
+        return True 
+       elif num%10==0:
+        return False 
+
+      
+       
+       return True
